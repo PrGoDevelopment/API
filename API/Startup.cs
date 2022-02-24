2b65f5638af200d9d7db650f28f2f7f95c5e3b83
Péricles Gomes <86753891+PrGoDevelopment@users.noreply.github.com>
2022-02-24 14:32:20 -0300

    Autorização desabilitada
    Teste.

--- API/Startup.cs
+++ API/Startup.cs
@@ -53,7 +53,7 @@ namespace API
 
             app.UseRouting();
 
-            app.UseAuthorization();
+            //app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
